8a296b976df986bec5fcc563f4fcbe270865134f
Noel J. Bergman <noel@devtech.com>
2026-01-13 20:22:58 -0600

    Add a simplified self-populating menu using NavLink to the Catalog page to highlight how simple it can be when not hidden amongst lots of CSS.

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,3 +1,23 @@
+import { NavLink } from "react-router-dom";
+
 export default function Catalog() {
-  return <div>Catalog Goes HERE!</div>;
+  const links = [
+    { to: "/", label: "Home" },
+    { to: "/about", label: "About Us" },
+  ];
+
+  return (
+    <>
+      <div>Catalog Goes HERE!</div>
+      <nav>
+        <ul>
+          {links.map((link) => (
+            <li>
+              <NavLink to={link.to}>{link.label}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
 }
