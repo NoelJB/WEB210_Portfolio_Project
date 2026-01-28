0becf6e757b0eebcf21c47a8bf212d6c1c10a79c
Noel J. Bergman <noel@devtech.com>
2026-01-27 20:37:36 -0600

    Add support for cors to facilitate react development

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 import { message, MyResume } from "./data.js";
 
 const app = express();
 const port = 3000;
+
+// Setup CORS
+app.use(cors({"exposedHeaders": ["Location"]}))
 
 // Routing for our app page
 app.get("/", (req, res) => { res.send(`<HTML><HEAD><TITLE>Portfolio Home Page</TITLE></HEAD><BODY><p>${message}</p></BODY></HTML>`); });
