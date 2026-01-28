3bdc9db4d508fd41fec8c649dceda101d8611f83
Noel J. Bergman <noel@devtech.com>
2026-01-27 18:45:11 -0600

    Add response status and content-type checking.  Cut down on diagnostic console logging.

--- server/demo.js
+++ server/demo.js
@@ -1,9 +1,16 @@
 const url = "http://localhost:3000/api/resume";
 const response = await fetch(url);
-const resume = await response.json();
 
-console.log(`Response: ${response}`);
-console.log(`Resume: ${resume}`);
-console.log(resume.name);
-console.log(resume.linkedin);
-console.log(JSON.stringify(resume));
+if (response.ok) {
+    const contentType = response.headers.get('Content-Type');
+
+    // console.log(`Response: ${response}`);
+    // console.log(`Content-Type: ${contentType}`);
+
+    if (contentType.startsWith("application/json")) {
+        const resume = await response.json();
+        // console.log(`Resume (JSON): ${resume}`);
+        //console.log(`Resume: ${JSON.stringify(resume)}`);
+        console.log(`LinkedIn for ${resume.name} is ${resume.linkedin}`);
+    }
+}
